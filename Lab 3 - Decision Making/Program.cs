7a723d67095b922d7cafc95dd4b56d75b7295069
Erwin <coronele@hotmail.com>
2020-01-22 21:48:17 -0500

    Final version

--- Lab 3 - Decision Making/Program.cs
+++ Lab 3 - Decision Making/Program.cs
@@ -8,7 +8,8 @@ namespace Lab_3___Decision_Making
         {
             int userInput;
             string userName, userContinue="y";
-
+            
+            static bool IsOdd(int x)
             /* 
             
             IsOdd - method that takes an integer (x) and uses mod to see if the number is divided
@@ -18,8 +19,6 @@ namespace Lab_3___Decision_Making
             Returns true if integer mod 2 is 1 (odd)
 
             */
-            
-            static bool IsOdd(int x)
             {
                 if (x % 2 == 0)
                 {
@@ -40,52 +39,56 @@ namespace Lab_3___Decision_Making
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine("\n\nHello! Please enter your name");
+            
             Console.ForegroundColor = ConsoleColor.DarkGray;
             userName = Console.ReadLine();
 
-            while (userContinue == "y")
+            while (userContinue == "y") // program will run until user replies "n" to the repeat prompt.
             {
                 // Use provided name.  It's good customer service!  Now politely ask them for their number.
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write($"\n\nHello {userName}! Please enter an integer to evaluate: ");
+            
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 userInput = int.Parse(Console.ReadLine());
 
                 Console.ForegroundColor = ConsoleColor.White;
-
 
                 // Evaluate and output to console
                 if (IsOdd(userInput))
                 {
                     if (userInput > 60)
                     {
-                        Console.WriteLine($"{userInput}  Odd [60 and up]");
+                        Console.WriteLine($"{userInput}  Odd [60 and up]\n");
                     }
                     else
                     {
-                        Console.WriteLine($"{userInput}  Odd");
+                        Console.WriteLine($"{userInput}  Odd\n");
                     }
                 }
                 else
                 {
                     if ((userInput >= 2) && (userInput <= 25))
                     {
-                        Console.WriteLine($"{userInput}  Even and less than 25");
+                        Console.WriteLine($"{userInput}  Even and less than 25\n");
                     }
                     else if ((userInput >= 26) && (userInput <= 60))
                     {
-                        Console.WriteLine($"{userInput}  Even [26 thru 60]");
+                        Console.WriteLine($"{userInput}  Even [26 thru 60]\n");
                     }
                     else if (userInput > 60)
                     {
-                        Console.WriteLine($"{userInput}  Even [60 and up]");
+                        Console.WriteLine($"{userInput}  Even [60 and up]\n");
                     }
                 }
 
                 // That was fun. Would you like to play again?
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine($"Would you like to analyze another number, {userName}?");
+                Console.Write($"\nWould you like to analyze another number, {userName} [y/n]? ");
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.BackgroundColor = ConsoleColor.Black;
                 userContinue = Console.ReadLine().ToLower();
                 
                 // Mildly inform user of his "user error"
@@ -95,12 +98,13 @@ namespace Lab_3___Decision_Making
                     Console.BackgroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine($"I'm SO sorry {userName}, you did NOT provide a valid response.");
                     Console.Write($"Let's try this again. Would you like to check another number? [y/n] ");
+
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.BackgroundColor = ConsoleColor.Black;
                     userContinue = Console.ReadLine().ToLower();
                 }
-
             }
+            Console.WriteLine("\n\n");
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.BackgroundColor = ConsoleColor.DarkRed;
             Console.WriteLine($"Thank you {userName}, See you again soon!");
